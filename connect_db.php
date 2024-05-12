75737cd1e1154186656872d79a7f11e98123a5c8
rio <willinggod170@gmail.com>
2024-05-12 14:13:47 +0100

    staging new files

--- connect_db.php
+++ connect_db.php
@@ -1,11 +1,10 @@
-<?php # CONNECT TO MySQL DATABASE.
-
+<?php
+ # CONNECT TO MySQL DATABASE.
 # Connect/Link  on 'localhost' .
 
 $link = mysqli_connect('localhost','root','','CodeSpace'); 
   if (!$link) { 
  
   die('Could not connect to MySQL: ' .mysqli_connect_error()); 
-} 
-  echo 'Connected to the database successfully!';  
+}   
 ?>
--- src/footer.php
+++ src/footer.php
@@ -1,9 +1,13 @@
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" 
-integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" 
-crossorigin="anonymous"></script>
-    
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" 
-crossorigin="anonymous"></script>
-  </body>
+
+ </div>
+ 
+ <!-- Option 1: Bootstrap Bundle with Popper -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+-->
+</body>
 </html>
